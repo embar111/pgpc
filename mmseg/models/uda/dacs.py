bace24474618178df082c0889c5fca98abe8f1d5
embar111 <2460590576@qq.com>
2025-01-13 15:37:17 +0800

    first commit

---
+++ mmseg/models/uda/dacs.py
@@ -0,0 +1,439 @@
+# Obtained from: https://github.com/lhoyer/DAFormer
+# Modifications:
+# - Delete tensors after usage to free GPU memory
+# - Add HRDA debug visualizations
+# - Support ImageNet feature distance for LR and HR predictions of HRDA
+#
+# The ema model and the domain-mixing are based on:
+# https://github.com/vikolss/DACS
+
+import math
+import os
+import random
+from copy import deepcopy
+
+import mmcv
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+from timm.models.layers import DropPath
+from torch.nn.modules.dropout import _DropoutNd
+
+from mmseg.core import add_prefix
+from mmseg.models import UDA, HRDAEncoderDecoder, build_segmentor
+from mmseg.models.segmentors.hrda_encoder_decoder import crop
+from mmseg.models.uda.uda_decorator import UDADecorator, get_module
+from mmseg.models.utils.dacs_transforms import (denorm, get_class_masks,
+                                                get_mean_std, strong_transform)
+from mmseg.models.utils.visualization import subplotimg
+from mmseg.utils.utils import downscale_label_ratio
+
+from ..losses.contrastive_loss import contrastive_loss, multi_resolution_cl
+import torch.nn.functional as F
+from mmseg.ops import resize
+
+
+def _params_equal(ema_model, model):
+    for ema_param, param in zip(ema_model.named_parameters(),
+                                model.named_parameters()):
+        if not torch.equal(ema_param[1].data, param[1].data):
+            # print("Difference in", ema_param[0])
+            return False
+    return True
+
+
+def calc_grad_magnitude(grads, norm_type=2.0):
+    norm_type = float(norm_type)
+    if norm_type == math.inf:
+        norm = max(p.abs().max() for p in grads)
+    else:
+        norm = torch.norm(
+            torch.stack([torch.norm(p, norm_type) for p in grads]), norm_type)
+
+    return norm
+
+
+@UDA.register_module()
+class DACS(UDADecorator):
+
+    def __init__(self, **cfg):
+        super(DACS, self).__init__(**cfg)
+        self.local_iter = 0
+        self.max_iters = cfg['max_iters']
+        self.alpha = cfg['alpha']
+        self.pseudo_threshold = cfg['pseudo_threshold']
+        self.psweight_ignore_top = cfg['pseudo_weight_ignore_top']
+        self.psweight_ignore_bottom = cfg['pseudo_weight_ignore_bottom']
+        self.fdist_lambda = cfg['imnet_feature_dist_lambda']
+        self.fdist_classes = cfg['imnet_feature_dist_classes']
+        self.fdist_scale_min_ratio = cfg['imnet_feature_dist_scale_min_ratio']
+        self.enable_fdist = self.fdist_lambda > 0
+        self.mix = cfg['mix']
+        self.blur = cfg['blur']
+        self.color_jitter_s = cfg['color_jitter_strength']
+        self.color_jitter_p = cfg['color_jitter_probability']
+        self.debug_img_interval = cfg['debug_img_interval']
+        self.print_grad_magnitude = cfg['print_grad_magnitude']
+        self.lambda_t = cfg['lambda_t']
+        assert self.mix == 'class'
+
+        self.debug_fdist_mask = None
+        self.debug_gt_rescale = None
+
+        self.class_probs = {}
+        ema_cfg = deepcopy(cfg['model'])
+        self.ema_model = build_segmentor(ema_cfg)
+
+        if self.enable_fdist:
+            self.imnet_model = build_segmentor(deepcopy(cfg['model']))
+        else:
+            self.imnet_model = None
+
+    def get_ema_model(self):
+        return get_module(self.ema_model)
+
+    def get_imnet_model(self):
+        return get_module(self.imnet_model)
+
+    def _init_ema_weights(self):
+        for param in self.get_ema_model().parameters():
+            param.detach_()
+        mp = list(self.get_model().parameters())
+        mcp = list(self.get_ema_model().parameters())
+        for i in range(0, len(mp)):
+            if not mcp[i].data.shape:  # scalar tensor
+                mcp[i].data = mp[i].data.clone()
+            else:
+                mcp[i].data[:] = mp[i].data[:].clone()
+
+    def _update_ema(self, iter):
+        alpha_teacher = min(1 - 1 / (iter + 1), self.alpha)
+        for ema_param, param in zip(self.get_ema_model().parameters(),
+                                    self.get_model().parameters()):
+            if not param.data.shape:  # scalar tensor
+                ema_param.data = \
+                    alpha_teacher * ema_param.data + \
+                    (1 - alpha_teacher) * param.data
+            else:
+                ema_param.data[:] = \
+                    alpha_teacher * ema_param[:].data[:] + \
+                    (1 - alpha_teacher) * param[:].data[:]
+
+    def train_step(self, data_batch, optimizer, **kwargs):
+        """The iteration step during training.
+
+        This method defines an iteration step during training, except for the
+        back propagation and optimizer updating, which are done in an optimizer
+        hook. Note that in some complicated cases or models, the whole process
+        including back propagation and optimizer updating is also defined in
+        this method, such as GAN.
+
+        Args:
+            data (dict): The output of dataloader.
+            optimizer (:obj:`torch.optim.Optimizer` | dict): The optimizer of
+                runner is passed to ``train_step()``. This argument is unused
+                and reserved.
+
+        Returns:
+            dict: It should contain at least 3 keys: ``loss``, ``log_vars``,
+                ``num_samples``.
+                ``loss`` is a tensor for back propagation, which can be a
+                weighted sum of multiple losses.
+                ``log_vars`` contains all the variables to be sent to the
+                logger.
+                ``num_samples`` indicates the batch size (when the model is
+                DDP, it means the batch size on each GPU), which is used for
+                averaging the logs.
+        """
+
+        optimizer.zero_grad()
+        log_vars = self(**data_batch)
+        optimizer.step()
+
+        log_vars.pop('loss', None)  # remove the unnecessary 'loss'
+        outputs = dict(
+            log_vars=log_vars, num_samples=len(data_batch['img_metas']))
+        return outputs
+
+    def masked_feat_dist(self, f1, f2, mask=None):
+        feat_diff = f1 - f2
+        # mmcv.print_log(f'fdiff: {feat_diff.shape}', 'mmseg')
+        pw_feat_dist = torch.norm(feat_diff, dim=1, p=2)
+        # mmcv.print_log(f'pw_fdist: {pw_feat_dist.shape}', 'mmseg')
+        if mask is not None:
+            # mmcv.print_log(f'fd mask: {mask.shape}', 'mmseg')
+            pw_feat_dist = pw_feat_dist[mask.squeeze(1)]
+            # mmcv.print_log(f'fd masked: {pw_feat_dist.shape}', 'mmseg')
+        # If the mask is empty, the mean will be NaN. However, as there is
+        # no connection in the compute graph to the network weights, the
+        # network gradients are zero and no weight update will happen.
+        # This can be verified with print_grad_magnitude.
+        return torch.mean(pw_feat_dist)
+
+    def calc_feat_dist(self, img, gt, feat=None):
+        assert self.enable_fdist
+        # Features from multiple input scales (see HRDAEncoderDecoder)
+        if isinstance(self.get_model(), HRDAEncoderDecoder) and \
+                self.get_model().feature_scale in \
+                self.get_model().feature_scale_all_strs:
+            lay = -1
+            feat = [f[lay] for f in feat]
+            with torch.no_grad():
+                self.get_imnet_model().eval()
+                feat_imnet = self.get_imnet_model().extract_feat(img)
+                feat_imnet = [f[lay].detach() for f in feat_imnet]
+            feat_dist = 0
+            n_feat_nonzero = 0
+            for s in range(len(feat_imnet)):
+                if self.fdist_classes is not None:
+                    fdclasses = torch.tensor(
+                        self.fdist_classes, device=gt.device)
+                    gt_rescaled = gt.clone()
+                    if s in HRDAEncoderDecoder.last_train_crop_box:
+                        gt_rescaled = crop(
+                            gt_rescaled,
+                            HRDAEncoderDecoder.last_train_crop_box[s])
+                    scale_factor = gt_rescaled.shape[-1] // feat[s].shape[-1]
+                    gt_rescaled = downscale_label_ratio(
+                        gt_rescaled, scale_factor, self.fdist_scale_min_ratio,
+                        self.num_classes, 255).long().detach()
+                    fdist_mask = torch.any(gt_rescaled[..., None] == fdclasses,
+                                           -1)
+                    fd_s = self.masked_feat_dist(feat[s], feat_imnet[s],
+                                                 fdist_mask)
+                    feat_dist += fd_s
+                    if fd_s != 0:
+                        n_feat_nonzero += 1
+                    del fd_s
+                    if s == 0:
+                        self.debug_fdist_mask = fdist_mask
+                        self.debug_gt_rescale = gt_rescaled
+                else:
+                    raise NotImplementedError
+        else:
+            with torch.no_grad():
+                self.get_imnet_model().eval()
+                feat_imnet = self.get_imnet_model().extract_feat(img)
+                feat_imnet = [f.detach() for f in feat_imnet]
+            lay = -1
+            if self.fdist_classes is not None:
+                fdclasses = torch.tensor(self.fdist_classes, device=gt.device)
+                scale_factor = gt.shape[-1] // feat[lay].shape[-1]
+                gt_rescaled = downscale_label_ratio(gt, scale_factor,
+                                                    self.fdist_scale_min_ratio,
+                                                    self.num_classes,
+                                                    255).long().detach()
+                fdist_mask = torch.any(gt_rescaled[..., None] == fdclasses, -1)
+                feat_dist = self.masked_feat_dist(feat[lay], feat_imnet[lay],
+                                                  fdist_mask)
+                self.debug_fdist_mask = fdist_mask
+                self.debug_gt_rescale = gt_rescaled
+            else:
+                feat_dist = self.masked_feat_dist(feat[lay], feat_imnet[lay])
+        feat_dist = self.fdist_lambda * feat_dist
+        feat_loss, feat_log = self._parse_losses(
+            {'loss_imnet_feat_dist': feat_dist})
+        feat_log.pop('loss', None)
+        return feat_loss, feat_log
+
+    def update_debug_state(self):
+        if self.local_iter % self.debug_img_interval == 0:
+            self.get_model().decode_head.debug = True
+            self.get_ema_model().decode_head.debug = True
+        else:
+            self.get_model().decode_head.debug = False
+            self.get_ema_model().decode_head.debug = False
+
+    def transform_inputs(self, inputs):
+        """Transform inputs for decoder.
+
+        Args:
+            inputs (list[Tensor]): List of multi-level img features.
+
+        Returns:
+            Tensor: The transformed inputs
+        """
+
+        inputs = [inputs[i] for i in [0, 1, 2, 3]]
+        upsampled_inputs = [
+            resize(
+                input=x,
+                size=inputs[0].shape[2:],
+                mode='bilinear',
+                align_corners=False) for x in inputs
+        ]
+        inputs = torch.cat(upsampled_inputs, dim=1)
+
+        return inputs
+
+    def forward_train(self,
+                     img,
+                     img_metas,
+                     gt_semantic_seg,
+                     target_img,
+                     target_img_metas,
+                     valid_pseudo_mask=None):
+        log_vars = {}
+        batch_size = img.shape[0]
+        dev = img.device
+
+        fm_size = [65, 65]
+
+        # Init/update ema model
+        if self.local_iter == 0:
+            self._init_ema_weights()
+            # assert _params_equal(self.get_ema_model(), self.get_model())
+
+        if self.local_iter > 0:
+            self._update_ema(self.local_iter)
+            # assert not _params_equal(self.get_ema_model(), self.get_model())
+            # assert self.get_ema_model().training
+        self.update_debug_state()
+        seg_debug = {}
+
+        means, stds = get_mean_std(img_metas, dev)
+        strong_parameters = {
+            'mix': None,
+            'color_jitter': random.uniform(0, 1),
+            'color_jitter_s': self.color_jitter_s,
+            'color_jitter_p': self.color_jitter_p,
+            'blur': random.uniform(0, 1) if self.blur else 0,
+            'mean': means[0].unsqueeze(0),  # assume same normalization
+            'std': stds[0].unsqueeze(0)
+        }
+
+        # Train on source images
+        clean_losses = self.get_model().forward_train(
+            img, img_metas, gt_semantic_seg, return_feat=True, return_fused=False)
+        src_feat = clean_losses.pop('features')
+
+        seg_debug['Source'] = self.get_model().decode_head.debug_output
+        clean_loss, clean_log_vars = self._parse_losses(clean_losses)
+        log_vars.update(clean_log_vars)
+        clean_loss.backward(retain_graph=self.enable_fdist)
+        if self.print_grad_magnitude:
+            params = self.get_model().backbone.parameters()
+            seg_grads = [
+                p.grad.detach().clone() for p in params if p.grad is not None
+            ]
+            grad_mag = calc_grad_magnitude(seg_grads)
+            mmcv.print_log(f'Seg. Grad.: {grad_mag}', 'mmseg')
+
+        # ImageNet feature distance
+        if self.enable_fdist:
+            feat_loss, feat_log = self.calc_feat_dist(img, gt_semantic_seg,
+                                                      src_feat)
+            log_vars.update(add_prefix(feat_log, 'src'))
+            # feat_loss.backward()
+            if self.print_grad_magnitude:
+                params = self.get_model().backbone.parameters()
+                fd_grads = [
+                    p.grad.detach() for p in params if p.grad is not None
+                ]
+                fd_grads = [g2 - g1 for g1, g2 in zip(seg_grads, fd_grads)]
+                grad_mag = calc_grad_magnitude(fd_grads)
+                mmcv.print_log(f'Fdist Grad.: {grad_mag}', 'mmseg')
+        del clean_loss
+
+        src_feat = self.transform_inputs(src_feat)
+        src_feat = F.interpolate(src_feat, size = fm_size,
+                                     mode = 'bilinear', align_corners=True)
+        lr_gt = F.interpolate(gt_semantic_seg.float(), size = fm_size,
+                           mode = 'nearest').long()
+        src_cl = contrastive_loss(src_feat,
+                                  lr_gt,
+                                 fdist_classes=self.fdist_classes)
+        src_res_cl_loss, src_cl_log = \
+            self._parse_losses({'src.explicit_cl_loss': src_cl})
+        encoder_loss = src_cl + feat_loss
+        encoder_loss.backward()
+        log_vars.update(src_cl_log)
+
+        del feat_loss, src_res_cl_loss
+
+        # Generate pseudo-label
+        for m in self.get_ema_model().modules():
+            if isinstance(m, _DropoutNd):
+                m.training = False
+            if isinstance(m, DropPath):
+                m.training = False
+        ema_logits = self.get_ema_model().generate_pseudo_label(
+            target_img, target_img_metas)
+        seg_debug['Target'] = self.get_ema_model().decode_head.debug_output
+
+        ema_softmax = torch.softmax(ema_logits.detach(), dim=1)
+        del ema_logits
+        pseudo_prob, pseudo_label = torch.max(ema_softmax, dim=1)
+        ps_large_p = pseudo_prob.ge(self.pseudo_threshold).long() == 1
+        ps_size = np.size(np.array(pseudo_label.cpu()))
+        pseudo_weight = torch.sum(ps_large_p).item() / ps_size * self.lambda_t
+        pseudo_weight = pseudo_weight * torch.ones(
+            pseudo_prob.shape, device=dev)
+        del pseudo_prob, ps_large_p, ps_size
+
+        if self.psweight_ignore_top > 0:
+            # Don't trust pseudo-labels in regions with potential
+            # rectification artifacts. This can lead to a pseudo-label
+            # drift from sky towards building or traffic light.
+            assert valid_pseudo_mask is None
+            pseudo_weight[:, :self.psweight_ignore_top, :] = 0
+        if self.psweight_ignore_bottom > 0:
+            assert valid_pseudo_mask is None
+            pseudo_weight[:, -self.psweight_ignore_bottom:, :] = 0
+        if valid_pseudo_mask is not None:
+            pseudo_weight *= valid_pseudo_mask.squeeze(1)
+        gt_pixel_weight = torch.ones((pseudo_weight.shape), device=dev)
+
+        # Apply mixing
+        mixed_img, mixed_lbl = [None] * batch_size, [None] * batch_size
+        mix_masks = get_class_masks(gt_semantic_seg)
+
+        for i in range(batch_size):
+            strong_parameters['mix'] = mix_masks[i]
+            mixed_img[i], mixed_lbl[i] = strong_transform(
+                strong_parameters,
+                data=torch.stack((img[i], target_img[i])),
+                target=torch.stack((gt_semantic_seg[i][0], pseudo_label[i])))
+            _, pseudo_weight[i] = strong_transform(
+                strong_parameters,
+                target=torch.stack((gt_pixel_weight[i], pseudo_weight[i])))
+        del gt_pixel_weight
+        mixed_img = torch.cat(mixed_img)
+        mixed_lbl = torch.cat(mixed_lbl)
+
+        # Train on mixed images
+        mix_losses = self.get_model().forward_train(
+            mixed_img, img_metas, mixed_lbl, pseudo_weight, return_feat=True,
+            return_fused=False)
+
+        mix_feat = mix_losses.pop('features')
+        mix_feat = self.transform_inputs(mix_feat)
+        mix_feat = F.interpolate(mix_feat, size=fm_size,
+                                 mode='bilinear', align_corners=True)
+
+        cl_pseudo_weight = F.interpolate(pseudo_weight.unsqueeze(1),
+                                         size=fm_size, mode='bilinear',
+                                         align_corners=True)
+        mix_lbl = F.interpolate(mixed_lbl.float(), size = fm_size,
+                                mode = 'nearest').long()
+
+
+        mix_cl = contrastive_loss(mix_feat,
+                                mix_lbl,
+                                pseudo_weight=cl_pseudo_weight,
+                                fdist_classes=self.fdist_classes)
+        mix_multi_res_cl_loss, mix_cl_log = self._parse_losses({'mix.explicit_cl_loss': mix_cl})
+        mix_multi_res_cl_loss.backward(retain_graph=True)
+
+        seg_debug['Mix'] = self.get_model().decode_head.debug_output
+        mix_losses = add_prefix(mix_losses, 'mix')
+        mix_loss, mix_log_vars = self._parse_losses(mix_losses)
+        log_vars.update(mix_log_vars)
+        mix_loss.backward()
+        log_vars.update(mix_cl_log)
+
+        del mix_lbl, mix_multi_res_cl_loss
+
+        self.local_iter += 1
+
+        return log_vars
